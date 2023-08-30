dd535ca9ef185571d79d7082811b023c660abe61
Robyn Smith <robynsmith21@hotmail.com>
2023-08-30 23:15:35 +0100

    added thought.js starter code

---
+++ models/Thought.js
@@ -0,0 +1,41 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('../models/Reaction');
+const moment = require('moment');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            trim: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        reactions: [reactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return `reactions: ${this.reactions.length}`;
+});
+
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
