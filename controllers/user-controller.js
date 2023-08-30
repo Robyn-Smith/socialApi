8024b50b82e87feeb8a019e72f54aa90ab296099
Robyn Smith <robynsmith21@hotmail.com>
2023-08-31 00:28:19 +0100

    error message update

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -26,7 +26,7 @@ const userController = {
       .select('-__v')
       .then(dbUserData => {
         if (!dbUserData) {
-          res.status(404).json({ message: 'No user found with this id!' });
+          res.status(404).json({ message: 'Error no user with this id' });
           return;
         }
         res.json(dbUserData);
@@ -49,7 +49,7 @@ const userController = {
     User.findOneAndUpdate({ _id: params.id }, body, { new: true, runValidators: true })
       .then(dbUserData => {
         if (!dbUserData) {
-          res.status(404).json({ message: 'No user found with this id!' });
+          res.status(404).json({ message: 'Error no user with this id' });
           return;
         }
         res.json(dbUserData);
@@ -62,7 +62,7 @@ const userController = {
     User.findOneAndDelete({ _id: params.id })
       .then(dbUserData => {
         if (!dbUserData) {
-          res.status(404).json({ message: 'No user found with this id!' });
+          res.status(404).json({ message: 'Error no user with this id' });
           return;
         }
         res.json(dbUserData);
