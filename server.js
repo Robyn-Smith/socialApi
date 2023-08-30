ef657f352b3863f52c31f230e848d14cc00d3e64
Robyn Smith <robynsmith21@hotmail.com>
2023-08-30 22:31:24 +0100

    added server.js file starter code

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+const routes = require('./routes');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-mediaAPI', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+mongoose.set('debug', true);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
